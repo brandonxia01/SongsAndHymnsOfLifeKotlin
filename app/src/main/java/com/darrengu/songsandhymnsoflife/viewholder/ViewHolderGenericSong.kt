a5451ec6406f20207184f16de57d94449c17b117
Yuanlong (Darren) Gu <darrengyl@gmail.com>
2018-04-26 16:01:41 -0500

    Move viewHolders

---
+++ app/src/main/java/com/darrengu/songsandhymnsoflife/viewholder/ViewHolderGenericSong.kt
@@ -0,0 +1,24 @@
+package com.darrengu.songsandhymnsoflife.viewholder
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.darrengu.songsandhymnsoflife.model.Song
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.adapter_item_song.*
+
+/**
+ * Created by darren.gu on 3/5/18.
+ */
+class ViewHolderGenericSong(override val containerView: View, private val onClickSong: (Long) -> Unit)
+    : RecyclerView.ViewHolder(containerView), LayoutContainer {
+    private var displayingSong: Song? = null
+
+    init {
+        containerView.setOnClickListener { displayingSong?.songId?.let { onClickSong(it) }}
+    }
+    fun bind(song: Song) {
+        displayingSong = song
+        songTrack.text = song.trackNumber.toString()
+        songTitle.text = song.title
+    }
+}
---
+++ app/src/main/java/com/darrengu/songsandhymnsoflife/viewholder/ViewHolderSubtitle.kt
@@ -0,0 +1,8 @@
+package com.darrengu.songsandhymnsoflife.viewholder
+
+import android.support.v7.widget.RecyclerView
+
+/**
+ * Created by darren.gu on 3/10/18.
+ */
+//class ViewHolderSubtitle : RecyclerView.ViewHolder
